a3ab0924a0d87923d0baaf43ec770c6a70caefb8
Diego Santos <diegosan.dev@gmail.com>
2024-11-04 14:56:32 -0300

    projeto base criado

---
+++ src/main/java/br/com/devspraticar/gestaousuario/GestaoUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devspraticar.gestaousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoUsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoUsuarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devspraticar/gestaousuario/GestaoUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devspraticar.gestaousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoUsuarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
